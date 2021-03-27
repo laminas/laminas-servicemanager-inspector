752e9ad246ba10bc47981d31f8ed3d1b667ad0c0
Artem Zakirullin <artemzr@gmail.com>
2021-03-27 09:18:27 +0200

    add laminas-cli

--- src/Command/InspectCommand.php
+++ src/Command/InspectCommand.php
@@ -8,4 +8,28 @@
 
 declare(strict_types=1);
 
-namespace Laminas\ServiceManagerInspector\Collector;
+namespace Laminas\ServiceManager\Inspector\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class InspectCommand extends Command
+{
+    public const HELP = <<<'EOH'
+TBD
+EOH;
+
+    public static $defaultName = 'servicemanager:inspect';
+
+    protected function configure()
+    {
+        $this->setDescription('Inspect ServiceManager configuration');
+        $this->setHelp(self::HELP);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        return 0;
+    }
+}
