87a40605b6b17fd6c3fecaac39cdc22bac3523ac
Artem Zakirullin <artemzr@gmail.com>
2021-05-29 17:01:58 +0300

    add color test

---
+++ test/EventReporter/ConsoleColor/ConsoleColorTest.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * @see       https://github.com/laminas/laminas-servicemanager-inspector for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-servicemanager-inspector/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-servicemanager-inspector/blob/master/LICENSE.md New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace LaminasTest\ServiceManager\Inspector\EventReporter\ConsoleColor;
+
+use Laminas\ServiceManager\Inspector\EventReporter\ConsoleColor\ConsoleColor;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Laminas\ServiceManager\Inspector\EventReporter\ConsoleColor\ConsoleColor
+ */
+class ConsoleColorTest extends TestCase
+{
+    public function testSuccessWrapsTextInGreenColor()
+    {
+        $color = new ConsoleColor();
+        $colored = $color->success('a');
+
+        $this->assertSame("\033[32ma\033[39m", $colored);
+    }
+
+    public function testNormalWrapsTextInWhiteColor()
+    {
+        $color = new ConsoleColor();
+        $colored = $color->normal('a');
+
+        $this->assertSame("\033[37ma\033[39m", $colored);
+    }
+
+    public function testWarningWrapsTextInYellownColor()
+    {
+        $color = new ConsoleColor();
+        $colored = $color->warning('a');
+
+        $this->assertSame("\033[33ma\033[39m", $colored);
+    }
+
+    public function testErrorWrapsTextInREdColor()
+    {
+        $color = new ConsoleColor();
+        $colored = $color->error('a');
+
+        $this->assertSame("\033[31ma\033[39m", $colored);
+    }
+
+    public function testCriticalWrapsTextInWhiteColorWithRedBackground()
+    {
+        $color = new ConsoleColor();
+        $colored = $color->critical('a');
+
+        $this->assertSame("\033[37;41ma\033[39;49m", $colored);
+    }
+}
