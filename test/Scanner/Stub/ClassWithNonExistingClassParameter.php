f5062853dae0cd5fab2c1914835a5d68cdb63e1c
Andi Rückauer <arueckauer@gmail.com>
2021-10-01 17:19:24 +0200

    Remove file headers
    Signed-off-by: Andi Rückauer <arueckauer@gmail.com>

--- test/Scanner/Stub/ClassWithNonExistingClassParameter.php
+++ test/Scanner/Stub/ClassWithNonExistingClassParameter.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas/laminas-servicemanager-inspector for the canonical source repository
- * @copyright https://github.com/laminas/laminas-servicemanager-inspector/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-servicemanager-inspector/blob/master/LICENSE.md New BSD License
- */
-
 declare(strict_types=1);
 
 namespace LaminasTest\ServiceManager\Inspector\Scanner\Stub;
